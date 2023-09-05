d9d97716a805a6e51f53745ca3429b72ca8d0b2e
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2023-09-05 12:07:02 -0700

    added method override

--- index.js
+++ index.js
@@ -42,20 +42,20 @@ app.get('/', (req, res) => {
   res.send("Welcome to my app");
 });
 
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-
-const bodyParser = require('body-parser'),
-methodOverride = require('method-override');
-
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
-app.use(bodyParser.json());
-app.use(methodOverride());
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
