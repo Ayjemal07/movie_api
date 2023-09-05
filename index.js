aa9d48aefc4816f1ce5ebe213824d02df100db26
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2023-09-04 22:07:14 -0700

    express and frameworks

--- index.js
+++ index.js
@@ -4,43 +4,49 @@ const express = require('express'),
   path = require('path');
 
 const app = express();
-// create a write stream (in append mode)
-// a ‘log.txt’ file is created in root directory
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-// setup the logger
+let movies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    director: 'Chris Columbus',
+    year: "2001"
+  },
+
+  {
+    title: "Singin' in the Rain",
+    director: "Gene Kelly",
+    year: "1951"
+  },
+  {
+    title: 'Lord of the Rings',
+    director: 'Peter Jackson',
+    year: "2001"
+  },
+  {
+    title: 'Twilight',
+    director: 'Catherine Hardwicke',
+    year: "2008"
+  }
+];
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
 
 app.get('/movies', (req, res) => {
-  res.send('Welcome to my app!');
+  res.json(movies);
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+app.get('/', (req, res) => {
+  res.send("Welcome to my app");
 });
-
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
-
-const bodyParser = require('body-parser'),
-methodOverride = require('method-override');
-
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-app.use((err, req, res, next) => {
-console.error(err.stack);
-res.status(500).send('Something broke!');
 });
 
-});
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
 });
